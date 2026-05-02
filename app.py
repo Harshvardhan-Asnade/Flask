b8f6d524e2fa349111ecee0ebc163db158fec12c
Harshvardhan Asnade <harshasnade1@gmail.com>
2026-05-02 05:48:11 +0530

    add search,trailer,poster,detailed page for movie

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import requests
 
 app = Flask(__name__)
 
+
+
 def fetch_movies():
     url = "https://api.themoviedb.org/3/trending/movie/week"
     params={
@@ -11,25 +13,51 @@ def fetch_movies():
     response=requests.get(url,params=params)
     data=response.json()
     return data["results"]
+def fetch_movie_detail(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    params = {"api_key": "5e4680fa8acf126d39b554e2d1733db0"}
 
-@app.route('/',methods=['GET','POST'])
+    response = requests.get(url, params=params)
+    return response.json()
+@app.route('/', methods=['GET', 'POST'])
 def home():
     movies = fetch_movies()
-    filtered_movies=movies
-    if request.method=='POST':
-        search=request.form.get('search')
-        filtered_movies=[]
-        for movie in movies.values():
-             if search.lower() in movie["title"].lower():
-                 filtered_movies.append(movie)
-    return render_template('index.html', movies=filtered_movies)
+
+    if request.method == 'POST':
+        search = request.form.get('search')
+
+        if search:   
+            movies = search_movies(search)
+
+    return render_template('index.html', movies=movies)
 @app.route('/movies/<int:id>')
 def movie_detail(id):
-    movie=movies.get(id)
+    movie=fetch_movie_detail(id)
+    videos = fetch_videos(id)
     if movie:
-        return render_template('movie_detail.html',movie=movie)
-    return render_template('404.html')
+        return render_template('movie_detail.html', movie=movie, videos=videos)
+    
+def search_movies(query):
+    url = "https://api.themoviedb.org/3/search/movie"
+    params = {
+        "api_key": "5e4680fa8acf126d39b554e2d1733db0",
+        "query": query
+    }
 
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    return data.get("results", [])
+
+def fetch_videos(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos"
+    params = {"api_key": "5e4680fa8acf126d39b554e2d1733db0"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    return data.get("results", [])
 
 if __name__=='__main__':
-    app.run(debug=True,port=3000)
+    app.run(debug=True,port=9000)
